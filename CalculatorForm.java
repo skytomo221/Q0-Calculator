10663a9d8c7cdc82bc4e505935301cc7987c93ef
skytomo <tomo1kawa.fullerene@gmail.com>
2019-11-09 21:44:01 +0900

    Add tabbed pane

--- CalculatorForm.java
+++ CalculatorForm.java
@@ -1,7 +1,7 @@
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,7 +9,9 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
+import javax.swing.WindowConstants;
 
 class CalculatorForm extends JFrame {
     /**
@@ -18,73 +20,63 @@ class CalculatorForm extends JFrame {
     private static final long serialVersionUID = 1L;
 
     private List<JButton> buttons;
+    private List<JButton> functionButtons;
+
+    GridBagLayout gbl = new GridBagLayout();
+    JTabbedPane tabbedPane = new JTabbedPane();
+    JPanel buttonPanel = new JPanel();
+    JPanel functionButtonPanel = new JPanel();
+    JTextPane inputTextPane = new JTextPane();
+    JTextPane logTextPane = new JTextPane();
 
     CalculatorForm() {
         super("FrameTest");
-        setSize(800, 400);
-        ButtonInitialization();
-        GridBagLayout gbl = new GridBagLayout();
+        setSize(600, 400);
         setLayout(gbl);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JPanel buttonPanel = new JPanel();
-        JTextPane inputTextPane = new JTextPane();
-        JTextPane logTextPane = new JTextPane();
+        addComportnent(inputTextPane, 0, 0, 2, 1, 2, 1);
+        addComportnent(tabbedPane, 0, 1, 2, 2, 2, 2);
+        addComportnent(logTextPane, 2, 0, 1, 3, 1, 3);
+
+        tabbedPane.add("標準", buttonPanel);
+        tabbedPane.add("関数", functionButtonPanel);
+
+        buttons = Arrays.asList(new JButton("AC"), new JButton("C"), new JButton("✗"), new JButton("÷"),
+                new JButton("7"), new JButton("8"), new JButton("9"), new JButton("×"), new JButton("4"),
+                new JButton("5"), new JButton("6"), new JButton("-"), new JButton("1"), new JButton("2"),
+                new JButton("3"), new JButton("+"), new JButton("+/-"), new JButton("0"), new JButton("."),
+                new JButton("="));
+        functionButtons = Arrays.asList(new JButton("or"), new JButton("xor"), new JButton("not"), new JButton("and"),
+                new JButton("mod"), new JButton("%"), new JButton("2nd"), new JButton("x^2"), new JButton("x^3"),
+                new JButton("x^y"), new JButton("e^x"), new JButton("10^x"), new JButton("1/x"), new JButton("√x"),
+                new JButton("3√x"), new JButton("y√x"), new JButton("ln"), new JButton("log10"), new JButton("x!"),
+                new JButton("sin"), new JButton("cos"), new JButton("tan"), new JButton("e"), new JButton("π"),
+                new JButton("rand"), new JButton("sinh"), new JButton("cosh"), new JButton("tanh"), new JButton("("),
+                new JButton(")"));
 
         for (JButton jButton : buttons) {
             buttonPanel.add(jButton);
         }
-        buttonPanel.setLayout(new GridLayout(5, 10));
+        buttonPanel.setLayout(new GridLayout(5, 4));
+        for (JButton jButton : functionButtons) {
+            functionButtonPanel.add(jButton);
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 2;
-        gbc.insets = new Insets(1, 1, 1, 1);
-        gbc.weightx = 1.0;
-        gbc.weighty = 2.0;
-        gbl.setConstraints(buttonPanel, gbc);
-        add(buttonPanel);
-
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.insets = new Insets(1, 1, 1, 1);
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-        gbl.setConstraints(inputTextPane, gbc);
-        add(inputTextPane);
-
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 3;
-        gbc.insets = new Insets(1, 1, 1, 1);
-        gbc.weightx = 1.0;
-        gbc.weighty = 2.0;
-        gbl.setConstraints(logTextPane, gbc);
-        add(logTextPane);
-
-        
+        }
+        functionButtonPanel.setLayout(new GridLayout(5, 6));
     }
 
-    private void ButtonInitialization() {
-        buttons = Arrays.asList(new JButton("or"), new JButton("xor"), new JButton("not"), new JButton("and"),
-                new JButton("mod"), new JButton("%"), new JButton("AC"), new JButton("C"), new JButton("✗"),
-                new JButton("÷"), new JButton("2nd"), new JButton("x^2"), new JButton("x^3"), new JButton("x^y"),
-                new JButton("e^x"), new JButton("10^x"), new JButton("7"), new JButton("8"), new JButton("9"),
-                new JButton("×"), new JButton("1/x"), new JButton("√x"), new JButton("3√x"), new JButton("y√x"),
-                new JButton("ln"), new JButton("log10"), new JButton("4"), new JButton("5"), new JButton("6"),
-                new JButton("-"), new JButton("x!"), new JButton("sin"), new JButton("cos"), new JButton("tan"),
-                new JButton("e"), new JButton("π"), new JButton("1"), new JButton("2"), new JButton("3"),
-                new JButton("+"), new JButton("rand"), new JButton("sinh"), new JButton("cosh"), new JButton("tanh"),
-                new JButton("("), new JButton(")"), new JButton("+/-"), new JButton("0"), new JButton("."),
-                new JButton("="));
+    void addComportnent(Component c, int x, int y, int w, int h, double wx, double wy) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = w;
+        gbc.gridheight = h;
+        gbc.weightx = wx;
+        gbc.weighty = wy;
+        gbl.setConstraints(c, gbc);
+        add(c);
     }
 
     private int getFontSizeAdjusted(JComponent component, String str, int sideMarginSize) {
