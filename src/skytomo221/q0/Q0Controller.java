26463501bb79c57fb5400338aa664b4f0ffd7136
skytomo <tomo1kawa.fullerene@gmail.com>
2019-12-05 00:11:17 +0900

    Cosmetic change ✨

--- src/skytomo221/q0/Q0Controller.java
+++ src/skytomo221/q0/Q0Controller.java
@@ -49,68 +49,82 @@ public class Q0Controller implements ActionListener, ComponentListener, Document
                     b.getMeaning().matches("\\d|¬|(?!or)(?!xor)(?!and)(?!mod)[A-Za-z]+")) {
                 viewer.emptyInput();
             }
-            if (b.getText().equals("C")) {
-                viewer.resetInput();
-            } else if (b.getText().equals("AC")) {
-                viewer.resetInput();
-                viewer.resetLog();
-            } else if (b.getText().equals("%")) {
-                viewer.insertToEndOfInput("% of ");
-            } else if (b.getMeaning().equals("reciprocal")) {
-                viewer.insertToStartOfInput("1/(");
-                viewer.insertToEndOfInput(")");
-            } else if (b.getMeaning().equals("radical-symbol")) {
-                viewer.insertToStartOfInput("pow(");
-                viewer.insertToEndOfInput(", 1/");
-            } else if (b.getText().equals("x!")) {
-                viewer.insertToEndOfInput("!");
-            } else if (b.getMeaning().equals("plus-and-minus")) {
-                viewer.invertSign();
-            } else if (b.getMeaning().equals("clear-symbol")) {
-                viewer.backspace();
-            } else if (b.getText().equals("=")) {
-                try {
-                    List<Token> tokens = lexer.parse(viewer.getTextOfInput());
-                    if (displayLexerResult) {
-                        System.out.println("[Lexer Log]");
-                        for (Token token : tokens) {
-                            System.out.println(token.toString());
+            switch (b.getMeaning()) {
+                case "C":
+                    viewer.resetInput();
+                    break;
+                case "AC":
+                    viewer.resetInput();
+                    viewer.resetLog();
+                    break;
+                case "%":
+                    viewer.insertToEndOfInput("% of ");
+                    break;
+                case "reciprocal":
+                    viewer.insertToStartOfInput("1/(");
+                    viewer.insertToEndOfInput(")");
+                    break;
+                case "radical-symbol":
+                    viewer.insertToStartOfInput("pow(");
+                    viewer.insertToEndOfInput(", 1/");
+                    break;
+                case "x!":
+                    viewer.insertToEndOfInput("!");
+                    break;
+                case "plus-and-minus":
+                    viewer.invertSign();
+                    break;
+                case "clear-symbol":
+                    viewer.backspace();
+                    break;
+                case "=":
+                    try {
+                        List<Token> tokens = lexer.parse(viewer.getTextOfInput());
+                        if (displayLexerResult) {
+                            System.out.println("[Lexer Log]");
+                            for (Token token : tokens) {
+                                System.out.println(token.toString());
+                            }
+                            System.out.println("");
                         }
-                        System.out.println("");
-                    }
-                    List<Expression> expressions = parser.parse(tokens);
-                    if (displayParserResult) {
-                        System.out.println("[Parser Log]");
-                        for (Expression expression : expressions) {
-                            System.out.println(expression.toString());
+                        List<Expression> expressions = parser.parse(tokens);
+                        if (displayParserResult) {
+                            System.out.println("[Parser Log]");
+                            for (Expression expression : expressions) {
+                                System.out.println(expression.toString());
+                            }
+                            System.out.println("");
                         }
-                        System.out.println("");
+                        calculator.calculate(expressions);
+                        if (displayCalculatorResult) {
+                            System.out.println("[Calculator Log]");
+                            System.out.println(calculator.getLog());
+                        }
+                        viewer.insertResultToLog(calculator.getAnswerToString());
+                    } catch (LexerException ex) {
+                        viewer.insertErrorToLog("[Lexer Error]", ex.getLexerErrorMessage());
+                        viewer.insertInstructionsHyperlinkToLog();
+                    } catch (ParserException ex) {
+                        viewer.insertErrorToLog("[Parser Error]", ex.getParserErrorMessage());
+                        viewer.insertInstructionsHyperlinkToLog();
+                    } catch (CalculatorException ex) {
+                        viewer.insertErrorToLog("[Calculator Error]", ex.getCalculatorErrorMessage());
+                        viewer.insertInstructionsHyperlinkToLog();
+                    } catch (Exception ex) {
+                        viewer.insertErrorToLog("[Java Error]", ex.getMessage() + "\n\n");
+                    } finally {
+                        viewer.setCaretOfLogToBottom();
                     }
-                    calculator.calculate(expressions);
-                    if (displayCalculatorResult) {
-                        System.out.println("[Calculator Log]");
-                        System.out.println(calculator.getLog());
-                    }
-                    viewer.insertResultToLog(calculator.getAnswerToString());
-                } catch (LexerException ex) {
-                    viewer.insertErrorToLog("[Lexer Error]", ex.getLexerErrorMessage());
-                    viewer.insertInstructionsHyperlinkToLog();
-                } catch (ParserException ex) {
-                    viewer.insertErrorToLog("[Parser Error]", ex.getParserErrorMessage());
-                    viewer.insertInstructionsHyperlinkToLog();
-                } catch (CalculatorException ex) {
-                    viewer.insertErrorToLog("[Calculator Error]", ex.getCalculatorErrorMessage());
-                    viewer.insertInstructionsHyperlinkToLog();
-                } catch (Exception ex) {
-                    viewer.insertErrorToLog("[Java Error]", ex.getMessage() + "\n\n");
-                } finally {
-                    viewer.setCaretOfLogToBottom();
-                }
-            } else if (b.getText().equals("+") || b.getText().equals("-") || b.getText().equals("×")
-                    || b.getText().equals("÷")) {
-                viewer.insertToEndOfInput(" " + b.getText() + " ");
-            } else {
-                viewer.insertToEndOfInput(b.getMeaning());
+                    break;
+                case "+":
+                case "-":
+                case "×":
+                case "÷":
+                    viewer.insertToEndOfInput(" " + b.getText() + " ");
+                    break;
+                default:
+                    viewer.insertToEndOfInput(b.getMeaning());
+                    break;
             }
         }
         viewer.setHighlighted(false);
